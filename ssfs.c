87b9e80dae655ae467562e1a3a57129cb43cceb6
crshao <calvindon08@gmail.com>
2020-04-27 10:07:56 +0700

    no4

--- ssfs.c
+++ ssfs.c
@@ -12,9 +12,9 @@
 #include<sys/wait.h>
 #include<stdbool.h>
 
-char dirpath[50] = "/home/rofita/Documents";
+char dirpath[50] = "/home/hao/Documents/Praktikum4";
 
-char ext[9999] = '\0';
+char ext[9999];
 int id = 0;
 
 void cek_sub(char *s, char *sub, int p, int l) {
@@ -25,6 +25,35 @@ void cek_sub(char *s, char *sub, int p, int l) {
       i++;
    }
    sub[i] = '\0';
+}
+
+void writeInfo(char *text, char* path)
+{
+    char* info = "INFO";
+	char curtime[30];
+    time_t t = time(NULL);
+    struct tm* p1 = localtime(&t);
+	strftime(curtime, 30, "%y%m%d-%H:%M:%S", p1);
+    char log[1000];
+    sprintf(log, "%s::%s::%s::%s", info, curtime, text, path);
+	FILE *out = fopen("/home/rofita/fs.log", "a");  
+    fprintf(out, "%s\n", log);  
+    fclose(out); 
+    
+}
+
+void writeWarning(char *text, char* path)
+{
+    char* info = "WARNING";
+    char curtime[30];
+    time_t t = time(NULL);
+    struct tm* p1 = localtime(&t);
+	strftime(curtime, 30, "%y%m%d-%H:%M:%S", p1);
+    char log[1000];
+    sprintf(log, "%s::%s::%s::%s", info, curtime, text, path);
+	FILE *out = fopen("/home/rofita/fs.log", "a");  
+    fprintf(out, "%s\n", log);  
+    fclose(out); 
 }
 
 char cis[] = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO";
@@ -327,6 +356,25 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		}
 	}
 	closedir(dp);
+    writeInfo("CD", fpath);
+	return 0;
+}
+
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	cekPath(fpath);
+	int res;
+	
+	if (S_ISREG(mode)) {
+		res = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0) res = close(res);
+	} else if (S_ISFIFO(mode)) res = mkfifo(fpath, mode);
+	else res = mknod(fpath, mode, rdev);
+	if (res == -1) return -errno;
+	
+    writeInfo("CREATE", fpath);
 	return 0;
 }
 
@@ -353,10 +401,21 @@ static int xmp_mkdir(const char *path, mode_t mode)
 	{
 		encrypt2(fpath, 1);
 	}
+	writeInfo("MKDIR", fpath);
 	return 0;
 }
 
+static int xmp_unlink(const char *path)
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	int res;
 
+	res = unlink(cekPath(fpath));
+    writeWarning("REMOVE", fpath);
+	if (res == -1) return -errno;
+	return 0;
+}
 
 static int xmp_rmdir(const char *path)
 {
@@ -370,6 +429,16 @@ static int xmp_rmdir(const char *path)
 	return 0;
 }
 
+static int xmp_symlink(const char *from, const char *to)
+{
+	int res;
+
+	res = symlink(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
 
 static int xmp_rename(const char *from, const char *to)
 {    
@@ -419,7 +488,73 @@ static int xmp_rename(const char *from, const char *to)
 	else if(from1 == 1 && too == 2) encrypt2(fto, 1);
 	else if(from1 == 2 && too != 1) encrypt1(fto, -1);
 	else if(from1 == 2 && too == 2) encrypt2(fto, 1);
+	writeInfo("RENAME", from2);
 
+	return 0;
+}
+
+static int xmp_link(const char *from, const char *to)
+{
+	int res;
+
+	res = link(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_chmod(const char *path, mode_t mode)
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	int res;
+
+	res = chmod(cekPath(fpath), mode);
+	writeInfo("CHMOD", fpath);
+	if (res == -1) return -errno;
+	return 0;
+}
+
+static int xmp_chown(const char *path, uid_t uid, gid_t gid)
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	int res;
+
+	res = lchown(cekPath(fpath), uid, gid);
+	writeInfo("CHOWN", fpath);
+	if (res == -1) return -errno;
+	return 0;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	int res;
+
+	res = truncate(cekPath(fpath), size);
+    writeInfo("TRUNCATE", fpath);
+	if (res == -1) return -errno;
+	return 0;
+}
+
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	int res;
+	struct timeval tv[2];
+
+	tv[0].tv_sec = ts[0].tv_sec;
+	tv[0].tv_usec = ts[0].tv_nsec / 1000;
+	tv[1].tv_sec = ts[1].tv_sec;
+	tv[1].tv_usec = ts[1].tv_nsec / 1000;
+
+	res = utimes(cekPath(fpath), tv);
+    writeInfo("UTIMENS", fpath);
+	if (res == -1) return -errno;
 	return 0;
 }
 
@@ -430,6 +565,7 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
 	int res;
 
 	res = open(cekPath(fpath), fi->flags);
+    writeInfo("OPEN", fpath);
 	if (res == -1) return -errno;
 	close(res);
 	return 0;
@@ -451,6 +587,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	if (res == -1) res = -errno;
 	close(fd);
 	
+    writeInfo("CAT", fpath);
 	return res;
 }
 
@@ -469,8 +606,52 @@ static int xmp_write(const char *path, const char *buf, size_t size,
 	res = pwrite(fd, buf, size, offset);
 	if (res == -1) res = -errno;
 
+    writeInfo("WRITE", fpath);
 	close(fd);
 	return res;
+}
+
+static int xmp_statfs(const char *path, struct statvfs *stbuf)
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+	int res;
+
+	res = statvfs(cekPath(fpath), stbuf);
+	if (res == -1) return -errno;
+	return 0;
+}
+
+static int xmp_create(const char* path, mode_t mode, struct fuse_file_info* fi) 
+{
+	char fpath[1000];
+	catPath(fpath, dirpath, path);
+    (void) fi;
+
+    int res;
+    res = creat(cekPath(fpath), mode);
+    if(res == -1) return -errno;
+	
+    writeInfo("CREAT", fpath);
+    close(res);
+    return 0;
+}
+
+
+static int xmp_release(const char *path, struct fuse_file_info *fi)
+{
+	(void) path;
+	(void) fi;
+	return 0;
+}
+
+static int xmp_fsync(const char *path, int isdatasync,
+		     struct fuse_file_info *fi)
+{
+	(void) path;
+	(void) isdatasync;
+	(void) fi;
+	return 0;
 }
 
 
@@ -481,12 +662,22 @@ static struct fuse_operations xmp_oper = {
 	.readdir	= xmp_readdir,
 	.mknod		= xmp_mknod,
 	.mkdir		= xmp_mkdir,
+	.symlink	= xmp_symlink,
+	.unlink		= xmp_unlink,
 	.rmdir		= xmp_rmdir,
 	.rename		= xmp_rename,
+	.link		= xmp_link,
+	.chmod		= xmp_chmod,
+	.chown		= xmp_chown,
+	.truncate	= xmp_truncate,
+	.utimens	= xmp_utimens,
 	.open		= xmp_open,
 	.read		= xmp_read,
 	.write		= xmp_write,
+	.statfs		= xmp_statfs,
 	.create     = xmp_create,
+	.release	= xmp_release,
+	.fsync		= xmp_fsync,
 };
 
 int main(int argc, char *argv[])
@@ -494,3 +685,4 @@ int main(int argc, char *argv[])
 	umask(0);
 	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
+
